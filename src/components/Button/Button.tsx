4cc3a7a2a7b1188dd36a06e396cd6236165d3aee
Emmanuel Ademuyiwa <techienelson@gmail.com>
2023-03-14 22:33:24 +0100

    Button component created and Navbar done for tab and mobile view

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="custom--button">
+      <h1>Get Started</h1>
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import LOGO from "../../assets/logo.svg";
+import MENU from "../../assets/menu.svg";
+import Button from "../Button/Button";
 
 const Navbar = () => {
   return (
     <div className="nav--bar">
-      <div className="logo--container">
-        <img src={LOGO} alt="logo-icon" />
+      <div className="logo--nav--container">
+        <div className="logo--nav">
+          <div className="logo--container">
+            <img src={LOGO} alt="logo icon" />
+          </div>
+
+          <div className="link--container">
+            <ul>
+              <li className="active--link">home</li>
+              <li>features</li>
+              <li>product</li>
+              <li>client</li>
+            </ul>
+          </div>
+        </div>
+
+        <Button />
       </div>
 
-      <div className="link--container">
-        <ul>
-          <li className="active--link">home</li>
-          <li>about us</li>
-          <li>features</li>
-          <li>solution</li>
-        </ul>
+      <div className="mobile--menu">
+        <img src={MENU} alt="mobile menu" />
       </div>
     </div>
   );
