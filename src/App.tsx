071e10f640c5c801dfaeb58a4e277fb4f7986edd
Emmanuel Ademuyiwa <techienelson@gmail.com>
2023-03-16 11:21:54 +0100

    Product section done

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CompanyInfo from "./components/CompanyInfo/CompanyInfo";
 import Features from "./components/Features/Features";
+import Product from "./components/Product/Product";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <CompanyInfo />
       <Features />
+      <Product />
     </div>
   );
 }
---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { bill, apple, google } from "../../assets";
+
+const Product = () => {
+  return (
+    <div className="product">
+      <div className="img--container">
+        <img src={bill} alt="" />
+      </div>
+      <div className="product--details">
+        <h1>Easily control your billing & invoicing.</h1>
+        <p>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className="img--flex">
+          <img src={apple} alt="apple icon" />
+          <img src={google} alt="google icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
